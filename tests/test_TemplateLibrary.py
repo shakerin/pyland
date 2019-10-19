b557f6580ab6edc78787303df59b99e08a1dbe61
Shakerin <shakerin.ahmed@gmail.com>
2019-10-19 15:22:41 +0800

    testcases developed to test all newly added functions in TemplateLibrary class including the ones to create frame object from frame directories dynamically

--- tests/test_TemplateLibrary.py
+++ tests/test_TemplateLibrary.py
@@ -54,7 +54,48 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_loadAllTemplates_frame_names(self):
         """check the internal list frame_name after creating TL object with path"""
-        ins = TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2"])
+        ins_4 = TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2"])
         expected_frame_names = ["test_file_1", "test_file_2", "test_file_3", "test_file_4", "test_file_5", "test_file_6"]
-        assert listCmp(expected_frame_names, ins.frame_names) == True
+        assert listCmp(expected_frame_names, ins_4.frame_names) == True
+
+    def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_name(self):
+        """test the internal variable 'name' that is stored in frame object
+        matches the file name string until file extension"""
+        ins_5= TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2"])
+        assert "test_file_1" == ins_5.test_file_1.name
+
+    def test_TemplateLibrary_loadAllTemplates_DynamicObjectsCreated(self):
+        """check which frame objects are created from the frame directory"""
+        ins_6= TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest"])
+        assert sorted(ins_6.frame_names) == sorted(["test_file_1", "test_file_2", "test_file_3"])
+
+    def test_TemplateLibrary_loadAllTemplates_ListOfFrameFiles(self):
+        """check the file paths of the created frame objects are correct"""
+        ins_8= TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest"])
+        dirpath = "/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest/"
+        filepaths = [
+            dirpath + "test_file_1.txt",
+            dirpath + "test_file_2.txt",
+            dirpath + "test_file_3",
+        ]
+        assert sorted(ins_8.frame_files)==sorted(filepaths)
+
+    def test_TemplateLibrary_loadAllTemplates_DynamicObjects_FrameLocationSearch(self):
+        """check the method for searching particular frame object is correct"""
+        ins_9 = TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2"])
+        assert ins_9.findFrame("test_file_6") == "/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2/test_file_6" 
+
+    def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_key_words(self):
+        """test the internal variable 'key_words' that is stored in frame object
+        matches the actual number of keywords in frame file"""
+        ins_5= TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest2"])
+        assert sorted(ins_5.test_file_1.key_words) == sorted(["number", "name"])
+
+    def test_TemplateLibrary_loadAllTemplates_DynamicObjectsUsage(self):
+        """check the generated text from particular frame object is correct"""
+        ins_7= TL(["/mnt/c/work/py-work/tests/testdir_TemplateLibraryTest"])
+        generated_code = ins_7.test_file_2.getGeneratedCode([('word','FILE'),('check','TEST')])
+        assert generated_code == "this is single FILE framefile just to TEST"
+
+
 
