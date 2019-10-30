a66cd84480e362b835c231fb3f7232e4521db144
shakerin <shakerin.ahmed@gmail.com>
2019-10-31 00:06:03 +0800

    tests added to check the executable segment extractor method is working fine

--- tests/test_top.py
+++ tests/test_top.py
@@ -7,18 +7,6 @@ import re
 from ucg.TemplateInfo import TemplateInfo as TI
 from ucg.FileToTemplate import FileToTemplate as FTT
 
-def listCmp(list1, list2):
-    match = True
-    if len(list1) == len(list2):
-        for i in list1:
-            if i in list2:
-                pass
-            else:
-                match = False
-                break
-    else:
-        match = False
-    return match
 
 class TestTop:
     """test name format : test_{className/methodName}_{unit_for_test}"""
@@ -30,8 +18,66 @@ class TestTop:
     def test_TemlateInfo_key_words(self):
         """internal variable key_word check"""
         ins = TI("ins2", "This is a $frame $string")
-        assert listCmp(ins.key_words, ['frame', 'string']) == True
-    
+        assert sorted(ins.key_words) == sorted(['frame', 'string'])
+
+    def test_TemlateInfo_block_identifier_default(self):
+        """internal variable block_identifier default value check"""
+        ins = TI("ins2", "This is a $frame $string")
+        assert ins.block_identifier == ("<<<", ">>>")
+
+    def test_TemlateInfo_block_identifier_custom(self):
+        """internal variable block_identifier custom value check"""
+        ins = TI("ins2", "This is a $frame $string", block_identifier=("<___", "___>"))
+        assert ins.block_identifier == ("<___", "___>")
+
+    def test_TemlateInfo_block_identifier_custom_start_end_same(self):
+        """internal variable block_identifier custom value check when
+        start and end search terms are equal"""
+        ins = TI("ins2", "This is a $frame $string", block_identifier=("<___", "<___"))
+        assert ins.block_identifier == ("<<<", ">>>")
+
+    def test_TemlateInfo_exec_sections_noExecPresentInFrameString(self):
+        """internal variable exec_sections check"""
+        ins = TI("ins2", "This is a $frame $string")
+        assert ins.exec_sections == []
+
+    def test_TemlateInfo_exec_sections_oneExecPresentInFrameString(self):
+        """internal variable exec_sections check when one executable string present
+        in string"""
+        frame_string = "This is a $frame $string\n <<<\n this is a cmd line \n line 2 \n >>>"
+        ins = TI("ins2", frame_string)
+        assert sorted(ins.exec_sections) == sorted([" this is a cmd line \n line 2 \n"])
+
+    def test_TemlateInfo_exec_sections_multipleExecPresentInFrameString(self):
+        """internal variable exec_sections check when multiple executable string present
+        in string"""
+        frame_string = "This is a $frame $string\n <<<\n this is a cmd line \n line 2 \n >>>\n this is it \n<<<\n this is a cmd line \n line 4 \n >>>"
+        ins = TI("ins2", frame_string)
+        assert sorted(ins.exec_sections) == sorted([" this is a cmd line \n line 2 \n", " this is a cmd line \n line 4 \n"])
+
+    def test_TemlateInfo_exec_sections_multipleExecPresentInFrameString_customStartEnd(self):
+        """internal variable exec_sections check when multiple executable string present
+        in string, custom start...end terms provided"""
+        frame_string = "This is a $frame $string\n #start\n this is a cmd line \n line 2 \n #end\n this is it \n#start\n this is a cmd line \n line 4 \n #end"
+        ins = TI("ins2", frame_string, block_identifier=("#start", "#end"))
+        assert sorted(ins.exec_sections) == sorted([" this is a cmd line \n line 2 \n", " this is a cmd line \n line 4 \n"])
+
+    def test_TemlateInfo_exec_sections_multipleExecPresentInFrameString_customStartEnd_endmissing(self):
+        """internal variable exec_sections check when multiple executable string present
+        in string, custom start...end terms provided, end term missing"""
+        frame_string = "This is a $frame $string\n #start\n this is a cmd line \n line 2 \n \n this is it \n#start\n this is a cmd line \n line 4 \n #end"
+        ins = TI("ins2", frame_string, block_identifier=("#start", "#end"))
+        assert sorted(ins.exec_sections) == sorted([" this is a cmd line \n line 2 \n \n this is it \n#start\n this is a cmd line \n line 4 \n"])
+
+    def test_TemlateInfo_exec_sections_multipleExecPresentInFrameString_customStartEnd_startmissing(self):
+        """internal variable exec_sections check when multiple executable string present
+        in string, custom start...end terms provided, start term missing"""
+        frame_string = "This is a $frame $string\n \n this is a cmd line \n line 2 \n #end\n this is it \n#start\n this is a cmd line \n line 4 \n #end"
+        ins = TI("ins2", frame_string, block_identifier=("#start", "#end"))
+        assert sorted(ins.exec_sections) == sorted([" this is a cmd line \n line 4 \n"])
+
+
+
     def test_TemlateInfo_Generated_code(self):
         """generated code from the template check"""
         ins = TI("ins3","I am $name, Who are $you. $name is me.")
@@ -50,7 +96,7 @@ class TestTop:
         """check if the class variable names stores all instance names properly"""
         instances = TI.names + ["nightmare"]
         ins1 = TI("nightmare","I am $name, Who are $you. $name is me.")
-        assert listCmp(TI.names,instances) == True
+        assert sorted(TI.names) == sorted(instances)
 
     def test_FileToTemplate_file_path(self):
         """check if the file_path variable is stored properly"""
@@ -71,7 +117,7 @@ class TestTop:
         """internal variable key_words value check"""
         filepath = "/mnt/c/work/py-land/pyland/tests/test_frame_file_1.txt"
         ins = FTT("instance1", filepath)
-        assert listCmp(ins.key_words,['something', 'mood']) == True
+        assert sorted(ins.key_words) == sorted(['something', 'mood'])
 
     def test_FileToTemplate_Generated_code(self):
         """check generated code is okay"""
@@ -98,7 +144,7 @@ class TestTop:
         filepath = "/mnt/c/work/py-land/pyland/tests/test_frame_file_1.txt"
         instances = TI.names + ["drStrange"]
         ins1 = FTT("drStrange", filepath)
-        assert listCmp(TI.names,instances) == True
+        assert sorted(TI.names) == sorted(instances)
 
     def test_FileToTemplate_Generated_code_DefaultValueOfKeyWords(self):
         """check generated code is okay when argument is missing for frames"""
