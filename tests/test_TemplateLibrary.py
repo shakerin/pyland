509af940d152ee4dfd5df1c5d3ca2bbe51830a16
shakerin <shakerin.ahmed@gmail.com>
2019-11-02 13:57:50 +0800

    removed listCmp method usage along with the method, cleaned up absolute directory path usage

--- tests/test_TemplateLibrary.py
+++ tests/test_TemplateLibrary.py
@@ -10,18 +10,8 @@ from ucg.TemplateInfo import TemplateInfo as TI
 from ucg.FileToTemplate import FileToTemplate as FTT
 from ucg.TemplateLibrary import TemplateLibrary as TL
 
-def listCmp(list1, list2):
-    match = True
-    if len(list1) == len(list2):
-        for i in list1:
-            if i in list2:
-                pass
-            else:
-                match = False
-                break
-    else:
-        match = False
-    return match
+testdir_TemplateLibraryTest = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"
+testdir_TemplateLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"
 
 class TestTemplateLibrary:
     """test name format : test_{className/methodName}_{unit_for_test}"""
@@ -30,28 +20,28 @@ class TestTemplateLibrary:
         """check the internal list frame_dirs after calling loadFrameFilesFromDir(dir_path)
         method from the object"""
         ins = TL()
-        ins.loadFrameFilesFromDir("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest")
+        ins.loadFrameFilesFromDir(testdir_TemplateLibraryTest)
         expected_frame_dirs = []
         assert expected_frame_dirs == ins.frame_dirs
 
     def test_TemplateLibrary_frame_dirs(self):
         """this test checks if the frame_dirs are stored properly or not."""
-        ins_22= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        ins_22.addFrameDirs(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins_22= TL([testdir_TemplateLibraryTest])
+        ins_22.addFrameDirs([testdir_TemplateLibraryTest2])
         exp_frame_dirs = [
-            "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest",
-            "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"
+            testdir_TemplateLibraryTest,
+            testdir_TemplateLibraryTest2
         ]
         assert sorted(ins_22.frame_dirs)==sorted(exp_frame_dirs)
 
     def test_TemplateLibrary_frame_dirs_CheckOneDirIsNotAddedTwice(self):
         """this test checks if the frame_dirs are stored properly or not."""
-        ins_22= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest", 
-                    "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
-        ins_22.addFrameDirs(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins_22= TL([testdir_TemplateLibraryTest, 
+                    testdir_TemplateLibraryTest2])
+        ins_22.addFrameDirs([testdir_TemplateLibraryTest2])
         exp_frame_dirs = [
-            "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest",
-            "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"
+            testdir_TemplateLibraryTest,
+            testdir_TemplateLibraryTest2
         ]
         assert sorted(ins_22.frame_dirs)==sorted(exp_frame_dirs)
 
@@ -59,71 +49,71 @@ class TestTemplateLibrary:
         """check the internal list frame_files after calling loadFrameFilesFromDir(dir_path)
         method from the object"""
         ins = TL()
-        ins.loadFrameFilesFromDir("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest")
+        ins.loadFrameFilesFromDir(testdir_TemplateLibraryTest)
         expected_list = ["test_file_1.txt", "test_file_2.txt", "test_file_3"]
-        expected_abspath_list = [join("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest", f) for f in expected_list] 
-        assert listCmp(expected_abspath_list, ins.frame_files) == True
+        expected_abspath_list = [join(testdir_TemplateLibraryTest, f) for f in expected_list] 
+        assert sorted(expected_abspath_list) == sorted(ins.frame_files)
 
     def test_TemplateLibrary_loadFrameFilesFromDir_frame_names(self):
         """check the internal list frame_files after calling loadFrameFilesFromDir(dir_path)
         method from the object"""
         ins = TL()
-        ins.loadFrameFilesFromDir("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest")
+        ins.loadFrameFilesFromDir(testdir_TemplateLibraryTest)
         expected_frame_names = ["test_file_1", "test_file_2", "test_file_3"]
-        assert listCmp(expected_frame_names, ins.frame_names) == True
+        assert sorted(expected_frame_names) == sorted(ins.frame_names)
 
     def test_TemplateLibrary_loadAllTemplates_frame_files(self):
         """check the internal list frame_files after creating TL object with path"""
-        ins = TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins = TL([testdir_TemplateLibraryTest,testdir_TemplateLibraryTest2])
         expected_list = ["test_file_1.txt", "test_file_2.txt", "test_file_3"]
-        expected_abspath_list = [join("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest", f) for f in expected_list] 
+        expected_abspath_list = [join(testdir_TemplateLibraryTest, f) for f in expected_list] 
         expected_list1 = ["test_file_4.txt", "test_file_5.txt", "test_file_6"]
-        expected_abspath_list1 = [join("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2", f) for f in expected_list1]         
+        expected_abspath_list1 = [join(testdir_TemplateLibraryTest2, f) for f in expected_list1]         
         final_expected_abspath_list = expected_abspath_list + expected_abspath_list1
-        assert listCmp(final_expected_abspath_list, ins.frame_files) == True
+        assert sorted(final_expected_abspath_list) == sorted(ins.frame_files)
 
     def test_TemplateLibrary_loadAllTemplates_frame_names(self):
         """check the internal list frame_name after creating TL object with path"""
-        ins_4 = TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins_4 = TL([testdir_TemplateLibraryTest,testdir_TemplateLibraryTest2])
         expected_frame_names = ["test_file_1", "test_file_2", "test_file_3", "test_file_4", "test_file_5", "test_file_6"]
-        assert listCmp(expected_frame_names, ins_4.frame_names) == True
+        assert sorted(expected_frame_names) == sorted(ins_4.frame_names)
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_name(self):
         """test the internal variable 'name' that is stored in frame object
         matches the file name string until file extension"""
-        ins_5= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins_5= TL([testdir_TemplateLibraryTest,testdir_TemplateLibraryTest2])
         assert "test_file_1" == ins_5.test_file_1.name
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectsCreated(self):
         """check which frame objects are created from the frame directory"""
-        ins_6= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
+        ins_6= TL([testdir_TemplateLibraryTest])
         assert sorted(ins_6.frame_names) == sorted(["test_file_1", "test_file_2", "test_file_3"])
 
     def test_TemplateLibrary_loadAllTemplates_ListOfFrameFiles(self):
         """check the file paths of the created frame objects are correct"""
-        ins_8= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        dirpath = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest/"
+        ins_8= TL([testdir_TemplateLibraryTest])
+        dirpath = testdir_TemplateLibraryTest
         filepaths = [
-            dirpath + "test_file_1.txt",
-            dirpath + "test_file_2.txt",
-            dirpath + "test_file_3",
+            dirpath + "/test_file_1.txt",
+            dirpath + "/test_file_2.txt",
+            dirpath + "/test_file_3",
         ]
         assert sorted(ins_8.frame_files)==sorted(filepaths)
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjects_FrameLocationSearch(self):
         """check the method for searching particular frame object is correct"""
-        ins_9 = TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
-        assert ins_9.findFrame("test_file_6") == "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/test_file_6" 
+        ins_9 = TL([testdir_TemplateLibraryTest,testdir_TemplateLibraryTest2])
+        assert ins_9.findFrame("test_file_6") == testdir_TemplateLibraryTest2 + "/test_file_6" 
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_key_words(self):
         """test the internal variable 'key_words' that is stored in frame object
         matches the actual number of keywords in frame file"""
-        ins_5= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest","/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"])
+        ins_5= TL([testdir_TemplateLibraryTest,testdir_TemplateLibraryTest2])
         assert sorted(ins_5.test_file_1.key_words) == sorted(["number", "name"])
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectsUsage(self):
         """check the generated text from particular frame object is correct"""
-        ins_7= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
+        ins_7= TL([testdir_TemplateLibraryTest])
         generated_code = ins_7.test_file_2.getGeneratedCode({
                                                              'word':'FILE',
                                                              'check':'TEST'
@@ -133,7 +123,7 @@ class TestTemplateLibrary:
     def test_TemplateLibrary_getGeneratedCode_DynamicObjectsUsage(self):
         """check the generated text from particular frame object is correct, this 
         getGenerateCode is accessible from TemplateLibrary"""
-        ins_7= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
+        ins_7= TL([testdir_TemplateLibraryTest])
         generated_code = ins_7.getGeneratedCode(ins_7.test_file_2,
                                                 {
                                                 'word':'FILE',
@@ -147,16 +137,16 @@ class TestTemplateLibrary:
     def test_TemplateLibrary_loadAllTemplates_addFrameDirs_FalseCheck(self):
         """check number of frames are different when all frame_dirs are not 
         included in the class object"""
-        ins_10= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        dirpath = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest/"
-        dirpath2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/"
+        ins_10= TL([testdir_TemplateLibraryTest])
+        dirpath = testdir_TemplateLibraryTest
+        dirpath2 = testdir_TemplateLibraryTest2
         filepaths = [
-            dirpath + "test_file_1.txt",
-            dirpath + "test_file_2.txt",
-            dirpath + "test_file_3",
-            dirpath2 + "test_file_4.txt",
-            dirpath2 + "test_file_5.txt",
-            dirpath2 + "test_file_6",
+            dirpath + "/test_file_1.txt",
+            dirpath + "/test_file_2.txt",
+            dirpath + "/test_file_3",
+            dirpath2 + "/test_file_4.txt",
+            dirpath2 + "/test_file_5.txt",
+            dirpath2 + "/test_file_6",
         ]
         assert sorted(ins_10.frame_files)!=sorted(filepaths)
 
@@ -164,17 +154,17 @@ class TestTemplateLibrary:
         """check number of frames are same when all frame_dirs are 
         included in the class object and thus the method addFrameDirs is
         working fine tested"""
-        ins_11= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        ins_11.addFrameDirs(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/"])
-        dirpath = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest/"
-        dirpath2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/"
+        ins_11= TL([testdir_TemplateLibraryTest])
+        ins_11.addFrameDirs([testdir_TemplateLibraryTest2])
+        dirpath = testdir_TemplateLibraryTest
+        dirpath2 = testdir_TemplateLibraryTest2
         filepaths = [
-            dirpath + "test_file_1.txt",
-            dirpath + "test_file_2.txt",
-            dirpath + "test_file_3",
-            dirpath2 + "test_file_4.txt",
-            dirpath2 + "test_file_5.txt",
-            dirpath2 + "test_file_6",
+            dirpath + "/test_file_1.txt",
+            dirpath + "/test_file_2.txt",
+            dirpath + "/test_file_3",
+            dirpath2 + "/test_file_4.txt",
+            dirpath2 + "/test_file_5.txt",
+            dirpath2 + "/test_file_6",
         ]
         assert sorted(ins_11.frame_files)==sorted(filepaths)
 
@@ -182,15 +172,15 @@ class TestTemplateLibrary:
         """check number of frames are same when all frame_dirs are 
         included in the class object and thus the method addFrameDirs is
         working fine tested"""
-        ins_12= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        ins_12.addFrameFromFile("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/test_file_4.txt")
-        dirpath = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest/"
-        dirpath2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/"
+        ins_12= TL([testdir_TemplateLibraryTest])
+        ins_12.addFrameFromFile(testdir_TemplateLibraryTest2 + "/test_file_4.txt")
+        dirpath = testdir_TemplateLibraryTest
+        dirpath2 = testdir_TemplateLibraryTest2
         filepaths = [
-            dirpath + "test_file_1.txt",
-            dirpath + "test_file_2.txt",
-            dirpath + "test_file_3",
-            dirpath2 + "test_file_4.txt",
+            dirpath + "/test_file_1.txt",
+            dirpath + "/test_file_2.txt",
+            dirpath + "/test_file_3",
+            dirpath2 + "/test_file_4.txt",
         ]
         assert sorted(ins_12.frame_files)==sorted(filepaths)
 
@@ -198,13 +188,15 @@ class TestTemplateLibrary:
         """check number of frames are same when all frame_dirs are 
         included in the class object and thus the method addFrameDirs is
         working fine tested"""
-        ins_13= TL(["/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"])
-        ins_13.addFrameFromFile("/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/test_file_10.txt")
-        dirpath = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest/"
-        dirpath2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2/"
+        ins_13= TL([testdir_TemplateLibraryTest])
+        ins_13.addFrameFromFile(testdir_TemplateLibraryTest2 + "/test_file_10.txt")
+        dirpath = testdir_TemplateLibraryTest
+        dirpath2 = testdir_TemplateLibraryTest2
         filepaths = [
-            dirpath + "test_file_1.txt",
-            dirpath + "test_file_2.txt",
-            dirpath + "test_file_3",
+            dirpath + "/test_file_1.txt",
+            dirpath + "/test_file_2.txt",
+            dirpath + "/test_file_3",
         ]
         assert sorted(ins_13.frame_files)==sorted(filepaths)
+
+
