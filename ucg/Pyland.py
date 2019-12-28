e00fad2848c2174a7e7620ad5b006ea43c735cb7
shakerin <shakerin.ahmed@gmail.com>
2019-12-29 01:23:03 +0800

    pyland class is the highest level or userend class

---
+++ ucg/Pyland.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+Pyland:
+
+Usage:
+	Pyland [argv]
+"""
+"""
+	Developer        : Shakerin Ahmed
+	Email            : shakerin.ahmed@gmail.com
+	Date             : December 28, 2019
+	Last Modified    : <source: git>
+	All Rights Reserved to Developer
+"""
+"""
+	Script Name: Pyland.py
+	Script Details: Universal Code
+"""
+
+
+from docopt import docopt
+import re
+from os import walk
+from os.path import join, isfile
+
+from .TemplateInfo import TemplateInfo as TI
+from .TemplateLibrary import TemplateLibrary as TL
+from .FileToTemplate import FileToTemplate as FTT
+from .Structure import Structure
+
+frame_dir = []
+
+
+
+
+class Pyland(object):
+	"""
+	- This class is the top most level of class in Pyland project.
+	- Every high level or user-end methods will be defined in this class.
+	- This class will keep the TemplateLibrary object for frameObj reference.
+	- It will have methods to automate Structure and Frames.
+	- Automating structure means, creating all files and directories based on
+	  structure file
+	- Automating frame means, executing frameObj. This can also be called 
+	  frame cmd.
+	"""
+	def __init__(self):
+		pass
+	
+	def createTemplateLibrary(self):
+		self.TL1 = TL(frame_dir)
+
+	def automateStructure(self):
+		self.St1 = Structure(struct_file)
+		self.St1.automate()
+		
+	def automateFrame(self):
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
