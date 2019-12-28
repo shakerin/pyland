8d39cd1779bb8b871539b2454e96017778332c79
shakerin <shakerin.ahmed@gmail.com>
2019-12-28 23:01:17 +0800

    test sample file directory structure improved

--- tests/path_variables.py
+++ tests/path_variables.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-testdir_TemplateLibraryTest = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest"
-testdir_TemplateLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/testdir_TemplateLibraryTest2"
-testdir_Discrete_Examples = "/mnt/c/work/py-land/pyland/tests/testdir_Discrete_Examples"
-testdir_Expected_Output_Examples = "/mnt/c/work/py-land/pyland/tests/testdir_Expected_Output_Examples"
-tests = "/mnt/c/work/py-land/pyland/tests/"
-testdir_CommandLibraryTest = "/mnt/c/work/py-land/pyland/tests/testdir_CommandLibraryTest"
-testdir_CommandLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/testdir_CommandLibraryTest2"
+testdir_TemplateLibraryTest = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_TemplateLibraryTest"
+testdir_TemplateLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_TemplateLibraryTest2"
+testdir_Discrete_Examples = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_Discrete_Examples"
+testdir_Expected_Output_Examples = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_Expected_Output_Examples"
+tests = "/mnt/c/work/py-land/pyland/tests/test_examples/"
+testdir_CommandLibraryTest = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_CommandLibraryTest"
+testdir_CommandLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_CommandLibraryTest2"
 
