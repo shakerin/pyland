8ef1dc9b02728a44aaf993fada2f1ed24c2c44a0
Shakerin <shakerin.ahmed@gmail.com>
2019-12-29 22:35:01 +0800

    all global variables should be capital letter for avoiding confusion

--- ucg/global_vars.py
+++ ucg/global_vars.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-frame_dir = []
+# by default some frame dirs may be provided
+DEFAULT_FRAME_DIR_LIST = []
+# user defined frame dirs
+USER_FRAME_DIR_LIST = []
+# user can define the struct file
+STRUCT_FILE = ""
+# user can define the frame command
+FRAME_CMD = ""
