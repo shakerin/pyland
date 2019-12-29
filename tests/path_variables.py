1508d1456cbf66934fd72540dca931cb23f5adf1
shakerin <shakerin.ahmed@gmail.com>
2019-12-29 21:45:19 +0800

    PROJECT_DIR variable added to make it easier for other users to run tests

--- tests/path_variables.py
+++ tests/path_variables.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
-testdir_TemplateLibraryTest = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_TemplateLibraryTest"
-testdir_TemplateLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_TemplateLibraryTest2"
-testdir_Discrete_Examples = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_Discrete_Examples"
-testdir_Expected_Output_Examples = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_Expected_Output_Examples"
-tests = "/mnt/c/work/py-land/pyland/tests/test_examples/"
-testdir_CommandLibraryTest = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_CommandLibraryTest"
-testdir_CommandLibraryTest2 = "/mnt/c/work/py-land/pyland/tests/test_examples/testdir_CommandLibraryTest2"
+PROJECT_DIR = "/mnt/c/work/py-land/"
+testdir_TemplateLibraryTest = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest"
+testdir_TemplateLibraryTest2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest2"
+testdir_Discrete_Examples = PROJECT_DIR + "pyland/tests/test_examples/testdir_Discrete_Examples"
+testdir_Expected_Output_Examples = PROJECT_DIR + "pyland/tests/test_examples/testdir_Expected_Output_Examples"
+tests = PROJECT_DIR + "pyland/tests/test_examples/"
+testdir_CommandLibraryTest = PROJECT_DIR + "pyland/tests/test_examples/testdir_CommandLibraryTest"
+testdir_CommandLibraryTest2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_CommandLibraryTest2"
 
