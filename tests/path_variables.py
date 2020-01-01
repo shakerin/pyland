cbe0ca10bf611f3fd81d6380069eaa8508081fd3
shakerin <shakerin.ahmed@gmail.com>
2020-01-01 19:37:48 +0800

    new variable added for common_func module

--- tests/path_variables.py
+++ tests/path_variables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 PROJECT_DIR = "/mnt/c/work/py-land/"
+testdir_common_func = PROJECT_DIR + "pyland/tests/test_examples/common_funcs"
 testdir_TemplateLibraryTest = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest"
 testdir_TemplateLibraryTest2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest2"
 testdir_Discrete_Examples = PROJECT_DIR + "pyland/tests/test_examples/testdir_Discrete_Examples"
