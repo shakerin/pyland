bcd265e0fc6d83ff2cebb7dbdddb7f4b55ee418e
shakerin <shakerin.ahmed@gmail.com>
2020-02-20 19:46:19 +0800

    TemplateLibrary paths updated with new directory

--- tests/path_variables.py
+++ tests/path_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 PROJECT_DIR = "/mnt/c/work/py-land/"
 PV_testdir_common_func = PROJECT_DIR + "pyland/tests/test_examples/common_funcs"
-PV_testdir_TemplateLibraryTest = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest"
-PV_testdir_TemplateLibraryTest2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibraryTest2"
+
 PV_testdir_Discrete_Examples = PROJECT_DIR + "pyland/tests/test_examples/testdir_Discrete_Examples"
 PV_testdir_Expected_Output_Examples = PROJECT_DIR + "pyland/tests/test_examples/testdir_Expected_Output_Examples"
 PV_tests = PROJECT_DIR + "pyland/tests/test_examples/"
@@ -14,6 +13,12 @@ PV_testdir_Frames = PROJECT_DIR + "pyland/tests/test_examples/FrameDir"
 
 PV_testdir_FilesToTemplate_FrameFiles = PROJECT_DIR + "pyland/tests/test_examples/testdir_FileToTemplate/FrameFiles"
 PV_testdir_FilesToTemplate_FrameFilesOutput = PROJECT_DIR + "pyland/tests/test_examples/testdir_FileToTemplate/FrameFilesOutput"
+
+PV_testdir_TemplateLibrary_FrameFiles = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibrary/FrameFiles"
+PV_testdir_TemplateLibrary_FrameFilesOutput = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibrary/FrameFilesOutput"
+PV_testdir_TemplateLibrary_FrameFiles_2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibrary/FrameFiles_2"
+PV_testdir_TemplateLibrary_FrameFilesOutput_2 = PROJECT_DIR + "pyland/tests/test_examples/testdir_TemplateLibrary/FrameFilesOutput_2"
+
 
 
 # following directory name should be used in testcases, when test
