855a6943f822f2e8607ceb00030aaf17810e3742
shakerin <shakerin.ahmed@gmail.com>
2020-02-22 13:28:00 +0800

    cleaned up

--- tests/test_TemplateLibrary.py
+++ tests/test_TemplateLibrary.py
@@ -14,6 +14,43 @@ from tests.path_variables import *
 
 class TestTemplateLibrary:
     """test name format : test_{className/methodName}_{unit_for_test}"""
+    ins_TL_1 = TL([PV_testdir_TemplateLibrary_FrameFiles])
+    ins_TL_2 = TL([PV_testdir_TemplateLibrary_FrameFiles_2])
+    ins_TL_all = TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
+
+    def insTL1FrameInfos(self):
+        expected_list, filenames1 = [], []
+        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
+            filenames = files
+            for filename in files:
+                expected_list.append(os.path.join(root, filename))
+        return (expected_list, filenames)
+
+
+    def insTLAllFrameInfos(self):
+        expected_list, filenames1 = [], []
+        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
+            filenames1 = files
+            for filename in files:
+                expected_list.append(os.path.join(root, filename))
+        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles_2):
+            filename2 = files
+            for filename in files:
+                if filename not in filenames1:
+                    expected_list.append(os.path.join(root, filename))
+        filenames1.extend(filename2)
+        filenames = list(dict.fromkeys(filenames1))
+        return (expected_list, filenames)
+
+    def removeFileExtension(self, file_names):
+        expected_frame_names = []
+        for frame_name in file_names:
+            if "." in frame_name:
+                expected_frame_name = frame_name.split(".")[0]
+            else:
+                expected_frame_name = frame_name
+            expected_frame_names.append(expected_frame_name)
+        return expected_frame_names
 
     def test_TemplateLibrary_loadFrameFilesFromDir_frame_dirs(self):
         """check the internal list frame_dirs after calling loadFrameFilesFromDir(dir_path)
@@ -74,89 +111,44 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_loadAllTemplates_frame_files(self):
         """check the internal list frame_files after creating TL object with path"""
-        ins = TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles_2):
-            for filename in files:
-                if filename not in filenames1:
-                    expected_list.append(os.path.join(root, filename))
-        assert sorted(expected_list) == sorted(ins.frame_files)
+        ins_TL_all_framepaths, frame_names = self.insTLAllFrameInfos()
+        assert sorted(ins_TL_all_framepaths) == sorted(TestTemplateLibrary.ins_TL_all.frame_files)
 
     def test_TemplateLibrary_loadAllTemplates_frame_names(self):
         """check the internal list frame_name after creating TL object with path"""
-        ins_4 = TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-        newfiles = []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles_2):
-            for filename in files:
-                if filename not in filenames1:
-                    newfiles.append(filename)
-        filenames1.extend(newfiles)
-        expected_frame_names = []
-        for frame_name in filenames1:
-            if "." in frame_name:
-                expected_frame_name = frame_name.split(".")[0]
-            else:
-                expected_frame_name = frame_name
-            expected_frame_names.append(expected_frame_name)
-        assert sorted(expected_frame_names) == sorted(ins_4.frame_names)
+        file_paths, file_names = self.insTLAllFrameInfos()
+        expected_frame_names = self.removeFileExtension(file_names)
+        assert sorted(expected_frame_names) == sorted(TestTemplateLibrary.ins_TL_all.frame_names)
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_name(self):
         """test the internal variable 'name' that is stored in frame object
         matches the file name string until file extension"""
-        ins_5= TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
-        assert "test_file_1" == ins_5.test_file_1.name
+        assert "test_file_1" == TestTemplateLibrary.ins_TL_all.test_file_1.name
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectsCreated(self):
         """check which frame objects are created from the frame directory"""
-        ins_6= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
-        expected_frame_names = []
-        for frame_name in filenames1:
-            if "." in frame_name:
-                expected_frame_name = frame_name.split(".")[0]
-            else:
-                expected_frame_name = frame_name
-            expected_frame_names.append(expected_frame_name)
-        assert sorted(ins_6.frame_names) == sorted(expected_frame_names)
+        expected_list, filenames = self.insTL1FrameInfos()
+        expected_frame_names = self.removeFileExtension(filenames)
+        assert sorted(TestTemplateLibrary.ins_TL_1.frame_names) == sorted(expected_frame_names)
 
     def test_TemplateLibrary_loadAllTemplates_ListOfFrameFiles(self):
         """check the file paths of the created frame objects are correct"""
-        ins_8= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        dirpath = PV_testdir_TemplateLibrary_FrameFiles
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
-
-        assert sorted(ins_8.frame_files)==sorted(expected_list)
+        expected_list, filenames = self.insTL1FrameInfos()
+        expected_frame_names = self.removeFileExtension(filenames)
+        assert sorted(TestTemplateLibrary.ins_TL_1.frame_files)==sorted(expected_list)
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjects_FrameLocationSearch(self):
         """check the method for searching particular frame object is correct"""
-        ins_9 = TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
-        assert ins_9.findFrame("test_file_6") == PV_testdir_TemplateLibrary_FrameFiles_2 + "/test_file_6" 
+        assert TestTemplateLibrary.ins_TL_all.findFrame("test_file_6") == PV_testdir_TemplateLibrary_FrameFiles_2 + "/test_file_6" 
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectInternalVariable_key_words(self):
         """test the internal variable 'key_words' that is stored in frame object
         matches the actual number of keywords in frame file"""
-        ins_5= TL([PV_testdir_TemplateLibrary_FrameFiles,PV_testdir_TemplateLibrary_FrameFiles_2])
-        assert sorted(ins_5.test_file_1.key_words) == sorted(["number", "name"])
+        assert sorted(TestTemplateLibrary.ins_TL_all.test_file_1.key_words) == sorted(["number", "name"])
 
     def test_TemplateLibrary_loadAllTemplates_DynamicObjectsUsage(self):
         """check the generated text from particular frame object is correct"""
-        ins_7= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_7.test_file_2.getGeneratedCode({
+        generated_code = TestTemplateLibrary.ins_TL_1.test_file_2.getGeneratedCode({
                                                              'word':'FILE',
                                                              'check':'TEST'
                                                              })
@@ -165,8 +157,7 @@ class TestTemplateLibrary:
     def test_TemplateLibrary_getGeneratedCode_DynamicObjectsUsage(self):
         """check the generated text from particular frame object is correct, this 
         getGenerateCode is accessible from TemplateLibrary"""
-        ins_7= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_7.getGeneratedCode(ins_7.test_file_2,
+        generated_code = TestTemplateLibrary.ins_TL_1.getGeneratedCode(TestTemplateLibrary.ins_TL_1.test_file_2,
                                                 {
                                                 'word':'FILE',
                                                 'check':'TEST'
@@ -174,39 +165,13 @@ class TestTemplateLibrary:
                                                )
         assert generated_code == "this is single FILE framefile just to TEST"
 
-
-
-    def test_TemplateLibrary_loadAllTemplates_addFrameDirs_FalseCheck(self):
-        """check number of frames are different when all frame_dirs are not 
-        included in the class object"""
-        ins_10= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        dirpath = PV_testdir_TemplateLibrary_FrameFiles
-        dirpath2 = PV_testdir_TemplateLibrary_FrameFiles_2
-        filepaths = [
-            dirpath + "/test_file_1.txt",
-            dirpath + "/test_file_2.txt",
-            dirpath + "/test_file_3",
-            dirpath2 + "/test_file_4.txt",
-            dirpath2 + "/test_file_5.txt",
-            dirpath2 + "/test_file_6",
-        ]
-        assert sorted(ins_10.frame_files)!=sorted(filepaths)
-
     def test_TemplateLibrary_loadAllTemplates_addFrameDirs_TrueCheck(self):
         """check number of frames are same when all frame_dirs are 
         included in the class object and thus the method addFrameDirs is
         working fine tested"""
         ins_11= TL([PV_testdir_TemplateLibrary_FrameFiles])
         ins_11.addFrameDirs([PV_testdir_TemplateLibrary_FrameFiles_2])
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles_2):
-            for filename in files:
-                if filename not in filenames1:
-                    expected_list.append(os.path.join(root, filename))
+        expected_list, filenames = self.insTLAllFrameInfos()
         assert sorted(ins_11.frame_files)==sorted(expected_list)
 
     def test_TemplateLibrary_loadAllTemplates_addFrameFromFile_TrueCheck(self):
@@ -215,11 +180,7 @@ class TestTemplateLibrary:
         working fine tested"""
         ins_12= TL([PV_testdir_TemplateLibrary_FrameFiles])
         ins_12.addFrameFromFile(PV_testdir_TemplateLibrary_FrameFiles_2 + "/test_file_4.txt")
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
+        expected_list, filenames = self.insTL1FrameInfos()
         expected_list.append(PV_testdir_TemplateLibrary_FrameFiles_2 + "/test_file_4.txt")
         assert sorted(ins_12.frame_files)==sorted(expected_list)
 
@@ -229,19 +190,12 @@ class TestTemplateLibrary:
         working fine tested"""
         ins_13= TL([PV_testdir_TemplateLibrary_FrameFiles])
         ins_13.addFrameFromFile(PV_testdir_TemplateLibrary_FrameFiles_2 + "/test_file_10.txt")
-        dirpath = PV_testdir_TemplateLibrary_FrameFiles
-        dirpath2 = PV_testdir_TemplateLibrary_FrameFiles_2
-        expected_list, filenames1 = [], []
-        for root, dirs, files in os.walk(PV_testdir_TemplateLibrary_FrameFiles):
-            filenames1 = files
-            for filename in files:
-                expected_list.append(os.path.join(root, filename))
+        expected_list, filenames = self.insTL1FrameInfos()
         assert sorted(ins_13.frame_files)==sorted(expected_list)
 
     def test_TemplateLibrary_frameObj_withExecSegments_getGeneratedCode(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_14.getGeneratedCode(ins_14.frame_with_exec_seg_assign_var,
+        generated_code = TestTemplateLibrary.ins_TL_1.getGeneratedCode(TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_assign_var,
                                                  {
                                                   'name':'EXAMPLE',
                                                   'anything':'FILE',
@@ -256,15 +210,13 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_exec_sections(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        execSections = ins_14.frame_with_exec_seg_assign_var.exec_sections
+        execSections = TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_assign_var.exec_sections
         expected_output = ['self.txt = "I am a txt generated from $name class"\n']
         assert execSections == expected_output
 
     def test_TemplateLibrary_frameObj_withExecSegments_modified_string(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        modified_string = ins_14.frame_with_exec_seg_assign_var.modified_string
+        modified_string = TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_assign_var.modified_string
         output_file_path = PV_testdir_TemplateLibrary_FrameFilesOutput + \
                            "/example_frame_with_exec_seg_assign_var_modified_string.txt" 
         expected_output = ""
@@ -274,18 +226,16 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_runExecSection(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        output = ins_14.runExecSection('self.txt = "I am Executed Text"')
+        output = TestTemplateLibrary.ins_TL_1.runExecSection('self.txt = "I am Executed Text"')
         assert output == "I am Executed Text"
 
     def test_TemplateLibrary_frameObj_withExecSegments_runExecSections_1(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
         exec_strings_list = [
             'self.txt = "I am Executed Text 1"',
             'self.txt = "I am Executed Text 2"'
         ]
-        output = ins_14.runExecSections(exec_strings_list)
+        output = TestTemplateLibrary.ins_TL_1.runExecSections(exec_strings_list)
         expected_output = [
             'I am Executed Text 1',
             'I am Executed Text 2'
@@ -294,12 +244,11 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_runExecSections_2(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
         exec_strings_list = [
             'self.txt = "I am Executed Text 1"',
             'print("I am Executed Text 2")'
         ]
-        output = ins_14.runExecSections(exec_strings_list)
+        output = TestTemplateLibrary.ins_TL_1.runExecSections(exec_strings_list)
         expected_output = [
             'I am Executed Text 1',
             ''
@@ -308,8 +257,7 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_getAll_1(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_14.getAll(ins_14.frame_with_exec_seg_assign_var,
+        generated_code = TestTemplateLibrary.ins_TL_1.getAll(TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_assign_var,
                                       {
                                         'name':'EXAMPLE',
                                         'anything':'FILE',
@@ -324,8 +272,7 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_getAll_2(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_14.getAll(ins_14.frame_with_exec_seg_assign_var_version2,
+        generated_code = TestTemplateLibrary.ins_TL_1.getAll(TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_assign_var_version2,
                                       {
                                         'name':'EXAMPLE',
                                         'anything':'FILE',
@@ -340,8 +287,7 @@ class TestTemplateLibrary:
 
     def test_TemplateLibrary_frameObj_withExecSegments_modified_string_2(self):
         """check the generated text from particular frame object is correct"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        modified_string = ins_14.frame_with_exec_seg_assign_var_version2.modified_string
+        modified_string = TestTemplateLibrary.ins_TL_all.frame_with_exec_seg_assign_var_version2.modified_string
         output_file_path = PV_testdir_TemplateLibrary_FrameFilesOutput + \
                            "/example_frame_with_exec_seg_assign_var_modified_string_2.txt" 
         expected_output = ""
@@ -353,8 +299,7 @@ class TestTemplateLibrary:
     def test_TemplateLibrary_frameObj_withExecSegments_getAll_3(self):
         """check the generated text from particular frame object is correct
         in this frame, local and global variables are used"""
-        ins_14= TL([PV_testdir_TemplateLibrary_FrameFiles])
-        generated_code = ins_14.getAll(ins_14.frame_with_exec_seg_variable_check,
+        generated_code = TestTemplateLibrary.ins_TL_1.getAll(TestTemplateLibrary.ins_TL_1.frame_with_exec_seg_variable_check,
                                       {})
         output_file_path = PV_testdir_TemplateLibrary_FrameFilesOutput + \
                            "/example_frame_with_exec_seg_variable_check.txt" 
