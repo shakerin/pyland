5534afdde554b5a69f4da96857429c8827f223c4
shakerin <shakerin.ahmed@gmail.com>
2020-02-25 21:42:17 +0800

    add docstring

--- ucg/Pyland.py
+++ ucg/Pyland.py
@@ -33,14 +33,37 @@ from .common_func import *
 
 
 class Pyland(object):
-	"""
-	- This class is the top most level of class in Pyland project.
-	- Every high level or user-end methods will be defined in this class.
-	- It will have methods to automate Structure and Frames.
-	- Automating structure means, creating all files and directories based on
-	  structure file
-	- Automating frame means, executing frameObj. This can also be called 
-	  frame cmd.
+	"""Top level class for this tool. This class can be utilized
+	to generate code based on frame command and automate directory
+	structure creation based on structure file.
+
+	Class Attributes
+	----------------
+	N/A
+
+	Attributes
+	----------
+	frame_dirs : a list of str
+		each string in the list represents a directory path,
+		there can be any number of frame files in each of the
+		directories,
+		frame names has to be unique which means, frame file
+		names should be unique. Two frame files will not be
+		considered unique even if the file extensions are 
+		different.
+
+	TL1 : an object of TemplateLibrary class
+		this is the template library instance that is used for
+		executing frame files
+
+	ST1 : an object of Sturcture class (only for Sturture Execution)
+		this is a structure class for analyzing information
+		about the structure file that is provided
+
+	frame_generated_code : string (only for Frame Execution)
+		this string represents the generated text from after
+		executing the frame file
+
 	"""
 	def __init__(self, frame_dirs, filepath_or_frame, output_file=""):
 		self.frame_dirs = frame_dirs
@@ -55,6 +78,21 @@ class Pyland(object):
 	
 
 	def isStruct(self, file_path):
+		"""checking if the input provided is a structure file path or
+		frame command
+		
+		Parameter
+		---------
+		file_path : a string
+			this method will check if this string is an actual path or
+			frame command
+
+		Returns
+		-------
+		is_struct : a boolean
+			True if file_path is an actual file path,
+			False if file_path is not a file path
+		"""
 		is_struct = False
 		if isfile(file_path):
 			is_struct = True
@@ -63,28 +101,44 @@ class Pyland(object):
 
 
 	def automateStructure(self, struct_file):
+		"""This method creates directory structure based on provided
+		structure file
+
+		Parameter
+		---------
+		struct_file : string
+			this string represents path to the structure file
+		"""
 		self.ST1 = Structure(struct_file)
 		self.automate()
 
 	def automateFrame(self, frame_cmd, output_file=""):
+		"""This method executes the frame command provided as an 
+		input.
+
+		Parameter
+		---------
+		frame_cmd : string
+			this string represents a frame command.
+
+		output_file : string
+			if not empty, output of the executed frame command
+			will be copied in this file
+		"""
 		generated_code = self.execFileCmd(frame_cmd, output_file)
 		self.frame_generated_code = generated_code
 		
 		
 	def automate(self):
-		self.createDirsAndFiles()
+		"""This method creates directories based on the structure 
+		file and based on associated commands, all files will be
+		created.
+		"""
+		self.createDirs()
 		self.execCmds()
 		pass
 
 	
-	
-	def createDirsAndFiles(self):
-		"""this method will create all directories and files if not already
-		created"""
-		self.createDirs()
-		#self.createFiles()
-		return
-
 	def createDirs(self):
 		"""create all directories present in self.dir_paths list if the 
 		directories are not already created"""
@@ -92,24 +146,18 @@ class Pyland(object):
 			createDirIfNotPresent(path)
 		return
 
-	def createFiles(self):
-		"""create all files present in self.file_paths list if the
-		files are not already created"""
-		for path in self.ST1.file_paths:
-			createFileIfNotPresent(path)
-		return
-
 
 	def execCmds(self):
+		"""Executes only the FILE type commands based on the structure file"""
 		for cmd_tuple in self.ST1.commands:
 			cmd_type, path, cmd = cmd_tuple
 			if cmd_type == "FILE":
-				"""justText('')"""
 				self.execFileCmd(cmd, path)
-			else:
-				pass
+		return
 
 	def execFileCmd(self, cmd, path=""):
+		"""Executes cmd and writes output from executed command to path
+		if path is not empty"""
 		frame_ins_name, frame_args = self.cleanFileCmd(cmd)
 		frame_name = frame_ins_name.replace("self.TL1.", "")
 		if frame_name in self.TL1.frame_names:
@@ -122,6 +170,9 @@ class Pyland(object):
 
 
 	def cleanFileCmd(self, cmd):
+		"""Cleans command and returns frame name and arguments in
+		a way that will be evaluated in execFileCmd method
+		"""
 		if "{" in cmd:
 			frame_name = cmd.split("{")[0]
 			frame_args = cmd.split("{")[1].replace("}", "")
