77d047368765b8951b9d9ae532a09c997c46f71f
shakerin <shakerin.ahmed@gmail.com>
2020-02-28 01:27:43 +0800

    cleanup

--- ucg/Structure.py
+++ ucg/Structure.py
@@ -124,11 +124,23 @@ class Structure(object):
 
 
 	def __init__(self, struct_file):
+
+
 		self.directory_sign = "//"
+		
 		self.file_sign = ",,"
+		
 		self.struct_file_path = struct_file
+		
 		self.run()
+		
 		return
+
+
+
+
+
+
 
 
 
@@ -153,49 +165,132 @@ class Structure(object):
 				based on the formed dir and file paths
 
 		"""
+		
+		
 		self.getOriginalStructureInList()
 		self.extractDirFileCmdNames()
 		self.formPaths()
 		self.cleanCmds()
-		#self.createDirsAndFiles()
+
 		return
+
+
+
+
+
+
+
+
+
+
 
 	def formPaths(self):
 		"""this method, forms the file paths and dir paths based on the
 		   information that is extracted from structure file by
 		   using extractDirFileCmdNames method
 		"""
+
+
 		self.formDirPaths()
 		self.formFilePaths()
+
+
 		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	def cleanCmds(self):
+
+
 		all_commands = []
 		clean_dir_cmds, clean_file_cmds = self.separate_dir_n_filecmd()
+		
 		for i, cmd in enumerate(clean_dir_cmds):
+		
 			command = ("DIR", self.dir_paths[i], cmd.strip())
 			all_commands.append(command)
+		
+
 		for i, cmd in enumerate(clean_file_cmds):
+		
 			command = ("FILE", self.file_paths[i], cmd.strip())
 			all_commands.append(command)
+		
+		
 		self.commands = all_commands
+		
+		
 		return
+
+
+
+
+
+
+
+
+
+
+
+
 
 	def separate_dir_n_filecmd(self):
 		"""This method is going to be used for returning file and
 		dir commands separately
 		"""
+
+
 		clean_dir_cmds = []
 		clean_file_cmds = []
+
 		for i, cmd in enumerate(self.cmd_names):
+
 			if self.cmd_types[i] == "DIR":
+
 				clean_dir_cmds.append(cmd)
+
+
 			elif self.cmd_types[i] == "FILE":
+
 				clean_file_cmds.append(cmd)
+
+
 			else:
+
 				pass
+
+
+
 		return (clean_dir_cmds, clean_file_cmds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	def getOriginalStructureInList(self):
@@ -205,10 +300,26 @@ class Structure(object):
 		an object attribute named 'original_list',
 		this list doesn't contain any empty item.
 		"""
+
+
 		with open(self.struct_file_path, 'r') as f:
+
 			original_list = f.readlines()
+
+
 		self.original_list = list(filter(None, original_list))
+
+
 		return
+
+
+
+
+
+
+
+
+
 
 
 	def extractDirFileCmdName(self, str_to_parse, separator):
@@ -241,12 +352,29 @@ class Structure(object):
 			cmd_name is the string that represents the portion in
 			string that contains command information
 		"""
+
+
+
 		dir_file_name = str_to_parse.strip().split(separator)[0]
-		cmd_name = str_to_parse.strip().split(separator)[1]
+
+
 		dir_file_name_raw = str_to_parse.split(separator)[0]
 		dir_file_name_raw_clean = dir_file_name_raw.strip()
 		dir_file_name_pos = len(dir_file_name_raw)-len(dir_file_name_raw_clean)
+
+
+		cmd_name = str_to_parse.strip().split(separator)[1]		
+
+
+
 		return (dir_file_name, dir_file_name_pos, cmd_name)
+
+
+
+
+
+
+
 
 	def extractDirFileCmdNames(self):
 		"""this method separates directory, file and command strings in 
@@ -272,32 +400,81 @@ class Structure(object):
 				structure file,
 				len(positions)==len(file_n_dir_names)
 		"""
+
+
+
+
+
 		dir_names, file_names, file_n_dir_names, cmd_names, cmd_types = [], [], [], [], []
 		positions_dir_only, positions = [], []
+
+
 		for line in self.original_list:
+
 			if self.directory_sign in line:
+
 				dir_name, no_of_preceding_space, cmd_name = self.extractDirFileCmdName(line, self.directory_sign)
+
 				dir_names.append(dir_name)
 				positions_dir_only.append(no_of_preceding_space)
+
 				file_n_dir_names.append(dir_name)
 				positions.append(no_of_preceding_space)
+
 				cmd_types.append("DIR")
 				cmd_names.append(cmd_name)				
+
+
 			elif self.file_sign in line:
+
 				file_n_dir_name, no_of_preceding_space_all, cmd_name = self.extractDirFileCmdName(line, self.file_sign)
-				positions.append(no_of_preceding_space_all)	
-				file_n_dir_names.append(file_n_dir_name)
+
 				file_names.append(file_n_dir_name)
+
+				file_n_dir_names.append(file_n_dir_name)
+				positions.append(no_of_preceding_space_all)	
+
 				cmd_types.append("FILE")
 				cmd_names.append(cmd_name)
+
+
+
+
+
+
 		self.cmd_types = cmd_types
+
 		self.cmd_names = cmd_names
+
 		self.file_names = file_names			
+		
 		self.dir_names = dir_names
+		
 		self.positions_dir_only = positions_dir_only
+		
 		self.file_n_dir_names = file_n_dir_names
+		
 		self.positions = positions
+		
+		
 		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	def createPathFromPosition(self, paths_no, path_names, assume_path_is_file=False):
@@ -326,37 +503,117 @@ class Structure(object):
 			each string represents a dir or file path,
 			this list may contain duplicate items
 		"""
+
+
+
 		dir_paths = []
+
 		for path in paths_no:
+
 			abs_path = ""
+
 			for i in path:
+
 				abs_path += path_names[i]+"/"
+
+
 			if assume_path_is_file:
+
 				abs_path = abs_path[:-1]
+
+
 			dir_paths.append(abs_path)
+
+
 		if assume_path_is_file:
+
 			dir_paths = self.cleanPaths(dir_paths, self.file_names)
+
+
 		return dir_paths
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	def formDirPaths(self):
 		"""Forms all paths to dir based on self.dir_paths
 		
 		self.dir_paths contains all directory paths"""
+
+
 		paths_no = self.formPathsFromPosition(self.positions_dir_only)
+
+
+
 		self.dir_paths = self.createPathFromPosition(paths_no, self.dir_names)
+
+
+
 		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	def formFilePaths(self):
 		"""Forms all paths to dir based on self.file_paths
 		
 		self.file_paths contains all file paths"""
+
+
+
 		paths_no = self.formPathsFromPosition(self.positions)
+
+
 		abs_file_paths = self.createPathFromPosition(paths_no, self.file_n_dir_names, True)
 		abs_file_paths = getUniqueOrderedList(abs_file_paths)
 		abs_file_paths = getOnlyUniqueItems(abs_file_paths, self.dir_paths)
+
+
 		self.file_paths = [f[:-1] for f in abs_file_paths]
+
+
 		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	def cleanPaths(self, abs_file_paths, file_names):
@@ -384,15 +641,34 @@ class Structure(object):
 		----
 		the returned list may contain duplicate item
 		"""
+
+
+
 		clean_abs_file_paths = []
+
+
 		file_names = list(set(file_names))
 		file_name_regex = "/|".join(file_names)
 		file_name_regex = file_name_regex+"/" if file_name_regex != "" else "XXXXXXXXXXXXXXXXX"
+
+
 		for abs_file_path in abs_file_paths:
+
 			clean_file_name = ""
+
 			clean_file_name = re.sub(file_name_regex, '', abs_file_path) + "/"
+
 			clean_abs_file_paths.append(clean_file_name)
+
+
+
 		return clean_abs_file_paths
+
+
+
+
+
+
 
 	def formPathsFromPosition(self, positions):
 		"""returns list of paths from a list of positions
@@ -434,21 +710,43 @@ class Structure(object):
 				['dir1', 'file1.txt', 'dir2', 'dir3'], 
 				they can easily create the paths from paths_no
 		"""
+
+
+
+
 		paths_no = []
+
+
 		for i, position in enumerate(positions):
+
 			segment = list(reversed(positions[:i+1]))
 			a1 = segment[0]
 			path = []
+
+
 			for j, pos in enumerate(segment[1:]):
+
 				if len(segment) < 2:
 					pass
+
+
 				else:
 					a2 = pos
+
 					if a1>a2:
 						a1 = a2
 						path.append(len(segment[1:])-1-j)# path ==> [0, 2, 3]
+
+
+
 			path = list(reversed(path))
+
 			path.append(i)
+
 			paths_no.append(path)
+
+
+
+
 		return paths_no
 
