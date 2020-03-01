8bc899b355817f672b9bf92665764594bb365017
shakerin <shakerin.ahmed@gmail.com>
2020-03-01 17:59:58 +0800

    minor update

--- tests/test_Pyland.py
+++ tests/test_Pyland.py
@@ -263,6 +263,8 @@ class TestPyland:
         for created_file, output_file in zip(created_file_paths, output_file_paths):
             matched &= filecmp.cmp(created_file, output_file)
             if not matched:
+                with open(created_file) as f:
+                    print(f.read())
                 print(created_file.split('/')[-1])
                 break
         assert matched == True
