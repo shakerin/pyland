5f03b0b94694c293ef50438c95410c877bea39ae
shakerin <shakerin.ahmed@gmail.com>
2020-03-11 01:51:55 +0800

    license info updated

--- ucg/FileToTemplate.py
+++ ucg/FileToTemplate.py
@@ -4,18 +4,29 @@ FileToTemplate:
 
 Usage:
 	FileToTemplate [argv]
-"""
-"""
-	Developer        : Shakerin Ahmed
-	Email            : shakerin.ahmed@gmail.com
-	Date             : September 30, 2019
-	Last Modified    : <source: git>
-	All Rights Reserved to Developer
-"""
-"""
 	Script Name: FileToTemplate.py
 	Script Details: Universal Code
 """
+
+
+# License
+# Copyright (C) 2020  Shakerin Ahmed
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 
 
 from .common_func import *
--- ucg/Pyland.py
+++ ucg/Pyland.py
@@ -4,18 +4,27 @@ Pyland:
 
 Usage:
 	Pyland [argv]
-"""
-"""
-	Developer        : Shakerin Ahmed
-	Email            : shakerin.ahmed@gmail.com
-	Date             : December 28, 2019
-	Last Modified    : <source: git>
-	All Rights Reserved to Developer
-"""
-"""
 	Script Name: Pyland.py
 	Script Details: Universal Code
 """
+
+
+# License
+# Copyright (C) 2020  Shakerin Ahmed
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 from docopt import docopt
