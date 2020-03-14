3d95691ae20175b3c05149355451e62dbbf632b1
shakerin <shakerin.ahmed@gmail.com>
2020-03-15 03:37:23 +0800

    major directory structure change

--- tests/context.py
+++ tests/context.py
@@ -2,9 +2,9 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from ucg.common_func import *
-from ucg.TemplateInfo import TemplateInfo as TI
-from ucg.FileToTemplate import FileToTemplate as FTT
-from ucg.TemplateLibrary import TemplateLibrary as TL
-from ucg.Structure import Structure
-from ucg.PylandMaster import PylandMaster
+from pyland.ucg.common_func import *
+from pyland.ucg.TemplateInfo import TemplateInfo as TI
+from pyland.ucg.FileToTemplate import FileToTemplate as FTT
+from pyland.ucg.TemplateLibrary import TemplateLibrary as TL
+from pyland.ucg.Structure import Structure
+from pyland.ucg.PylandMaster import PylandMaster
