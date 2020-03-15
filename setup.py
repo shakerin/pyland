5708cce07cd3dbc82e25e0e4994ff01c3a6a0c6a
shakerin <shakerin.ahmed@gmail.com>
2020-03-15 15:12:36 +0800

    updated for creating .so file - package testing is still ongoing

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-import setuptools
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,17 +9,21 @@ with open("README.md", "r") as fh:
 with open('LICENSE') as f:
     license = f.read()
 
-setuptools.setup(
+ext_modules = [
+    Extension("pyland",  
+               sources = ["pyland/Pyland.py"]),
+]
+
+setup(
     name="pyland",
     version="0.0.1",
     author="Shakerin Ahmed",
     author_email="shakerin.ahmed@gmail.com",
     description="Universal Text Generator and Code Executor 2 in 1",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    #url="",
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
     license=license,
-    packages=setuptools.find_packages(exclude=('tests', 'docs')),
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
@@ -27,5 +34,4 @@ setuptools.setup(
         'Topic :: Utilities',
         'Topic :: Scientific/Engineering',
     ],
-    python_requires='>=3.6',
 )
