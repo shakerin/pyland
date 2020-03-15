500974f2612db34260a73a73330949e37842a030
Shakerin <shakerin.ahmed@gmail.com>
2020-03-15 12:27:02 +0800

    setup updated and working

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
 setuptools.setup(
     name="pyland",
     version="0.0.1",
     author="Shakerin Ahmed",
-    packages='ucg'.split(),
     author_email="shakerin.ahmed@gmail.com",
     description="Universal Text Generator and Code Executor 2 in 1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     #url="",
+    license=license,
+    packages=setuptools.find_packages(exclude=('tests', 'docs')),
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
